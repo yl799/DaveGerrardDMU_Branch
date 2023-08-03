ca2418e6a0476e05f2cf2decbf77f93998d83f13
yl799 <115647982+yl799@users.noreply.github.com>
2023-08-03 23:06:52 +0530

    Update ecl_parser.ml

--- ecl_parser.ml
+++ ecl_parser.ml
@@ -1,9 +1,14 @@
-(* ecl_parser.ml *)
-
-(* Define the ECL formula type *)
 type ecl_formula = string
 
-(* Parse an ECL formula *)
-let parse_ecl_formula (input: string) : ecl_formula =
-  (* Placeholder logic for parsing *)
-  input
+let parse_ecl_formula (input: string) : 
+  ecl_formula = input
+  
+  let parsed_tokens = String.split_on_char ' ' input in
+  String.concat "_" parsed_tokens  
+
+
+let () =
+  let input_formula = "F(G(p -> q))" in
+  let parsed_formula = parse_ecl_formula input_formula in
+  Printf.printf "Input Formula: %s\n" input_formula;
+  Printf.printf "Parsed Formula: %s\n" parsed_formula
