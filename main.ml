abd7bf815c526730796aed743cf40d2167b73e8e
yl799 <115647982+yl799@users.noreply.github.com>
2023-08-03 22:55:57 +0530

    Update main.ml

--- main.ml
+++ main.ml
@@ -1,18 +1,11 @@
-(* main.ml *)
-
-(* Import the modules *)
 open Ecl_parser
 open Timed_automata
 
-(* Main function *)
 let main () =
-  (* Example ECL formula *)
   let ecl_formula = Ecl_parser.parse_ecl_formula "ECL_formula_1" in
 
-  (* Translate and print the result *)
   let timed_automata = Timed_automata.translate_to_timed_automata ecl_formula in
   Printf.printf "ECL Formula: %s\n" ecl_formula;
   Printf.printf "Translated Timed Automata:\n%s\n" timed_automata
 
-(* Run the main function *)
 let () = main ()
